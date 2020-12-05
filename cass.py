dbbc7d248044ea32339d2a77fad806b17e9066ac
Bryant Hou <gbhou5@gmail.com>
2020-12-05 18:58:28 -0500

    DATABASE WORKING

--- cass.py
+++ cass.py
@@ -1,12 +1,13 @@
 from cassandra.cluster import Cluster
 from cassandra.auth import PlainTextAuthProvider
+import uuid
 
 cloud_config= {
         'secure_connect_bundle': 'secure-connect-HackDook.zip'
 }
-auth_provider = PlainTextAuthProvider('HackDook', 'DukeHack2020')
+auth_provider = PlainTextAuthProvider('x', 'password')
 cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
-session = cluster.connect('HackDook')
+session = cluster.connect('hackdook')
 
 row = session.execute("select release_version from system.local").one()
 if row:
@@ -15,11 +16,9 @@ else:
     print("An error occurred.")
 
 def predicted(text, score):
-    stmt = session.prepare(" INSERT INTO predicted (text, score) VALUES (?, ?)")
-    results = session.execute(stmt, [text, score])
+    stmt = session.prepare(" INSERT INTO hackdook.predicted (id, score, submitted) VALUES (?, ?, ?)")
+    results = session.execute(stmt, [uuid.uuid4(),  score, text])
 
 def user_generated(text, score):
-    stmt = session.prepare(" INSERT INTO user_generated (text, score) VALUES (?, ?)")
-    results = session.execute(stmt, [text, score])
-
-predicted('asd', 8)
+    stmt = session.prepare(" INSERT INTO hackdook.user_generated ( id, submitted, score) VALUES (?, ?, ?)")
+    results = session.execute(stmt, [uuid.uuid4(), text, score])

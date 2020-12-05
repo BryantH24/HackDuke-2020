6926526dc6e3628e110fa0d1dcc37adbe0ebf0e3
George Zhuang <xgz2@cornell.edu>
2020-12-05 17:18:33 -0500

    Correct backend setup, prepare for deploy

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
 import numpy as np
 from flask import Flask, request, jsonify
-import pickle
+from HateSpeechClassificationModel.classifier import get_tweets_predictions
+
 
 app = Flask(__name__)
-model = pickle.load(open('final_model.pkl', 'rb'))
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-@app.route('predict', methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
     examples = [x for x in request.form.values()]
     final_examples = [np.array(features)]
     predictions = model.get_tweets_predictions(final_examples, False)
 
     return predictions
+
+if __name__ == "__main__":
+    app.run(threaded=True, port=5000)

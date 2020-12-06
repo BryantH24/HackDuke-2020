e260061fe4715c96333500116c44f02d5031a7f3
George Zhuang <xgz2@cornell.edu>
2020-12-06 12:45:44 -0500

    Highlighting in real time

--- no-bad-word/src/pages/editor.js
+++ no-bad-word/src/pages/editor.js
@@ -1,11 +1,63 @@
 import React, { useState } from "react";
-import { Container, Form, Button } from "react-bootstrap";
+import { Container, Col, Button, Row } from "react-bootstrap";
 import "../styles/editor.css";
 import axios from "axios";
+import { HighlightWithinTextarea } from "react-highlight-within-textarea";
 
 export default function Editor() {
   const [state, setState] = useState("");
   const [counter, setCounter] = useState(0);
+  const [highlight, setHighlight] = useState([]);
+
+  const evaluateSentence = async (str) => {
+    var result = [];
+    await axios
+      .post("https://hackdook-udtyuwqxja-ue.a.run.app/label", {
+        text: str,
+      })
+      .then((res) => {
+        const label = res.data.label;
+        console.log(label.length);
+        result.push(label);
+      })
+      .catch((err) => {
+        console.log(err);
+        console.log("Error evaluating sentence");
+      });
+    return result;
+  };
+
+  const indexInput = (str, markerList) => {
+    var sentenceIndices = [];
+    for (var i = 0; i < str.length; i++) {
+      if (str[i] == ".") {
+        sentenceIndices.push(i);
+      }
+    }
+    var highlightSections = [];
+    for (var i = 0; i < sentenceIndices.length; i++) {
+      if (markerList[i][0] == 0 || markerList[i][0] == 1) {
+        if (i == 0) {
+          highlightSections.push([0, sentenceIndices[i]]);
+        } else {
+          highlightSections.push([
+            sentenceIndices[i - 1] + 1,
+            sentenceIndices[i],
+          ]);
+        }
+      }
+    }
+    if (
+      sentenceIndices[sentenceIndices.length - 1] < str.length - 1 &&
+      markerList[markerList.length - 1] < 2
+    ) {
+      highlightSections.push([
+        sentenceIndices[sentenceIndices.length - 1],
+        str.length - 1,
+      ]);
+    }
+    setHighlight(highlightSections);
+  };
 
   const handleChange = async (event) => {
     setState(event.target.value);
@@ -17,21 +69,12 @@ export default function Editor() {
       const result = [];
       var example;
       for (example of parsedSentences) {
-        console.log(example);
-        await axios
-          .post("https://hackdook-udtyuwqxja-ue.a.run.app/label", {
-            text: example,
-          })
-          .then((res) => {
-            result.push(res.data.label);
-            console.log(res);
-          })
-          .catch((err) => {
-            console.log(err);
-            console.log("Error evaluating sentence");
-          });
+        result.push(await evaluateSentence(example));
       }
+      indexInput(state, result);
+      console.log(parsedSentences);
       console.log(result);
+      console.log(highlight);
     }
   };
 
@@ -44,26 +87,12 @@ export default function Editor() {
   return (
     <Container fluid className="editor-container">
       <Container className="white-background-container">
-        <Form className="text-area-container">
-          <Form.Group controlId="text-input">
-            <Form.Control
-              as="textarea"
-              placeholder="Start typing..."
-              rows={20}
-              className="input-box"
-              value={state}
-              onChange={handleChange}
-            />
-          </Form.Group>
-          <Button
-            className="scan-button"
-            variant="primary"
-            type="submit"
-            onClick={handleScan}
-          >
-            SCAN
-          </Button>
-        </Form>
+        <HighlightWithinTextarea
+          value={state}
+          highlight={highlight}
+          onChange={handleChange}
+          className="input-box"
+        ></HighlightWithinTextarea>
       </Container>
     </Container>
   );

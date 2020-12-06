b60b651346637b45847a147d6b457b3da358a0e7
Supratik Pochampally <supratikpochampally@gmail.com>
2020-12-05 18:49:03 -0600

    front end test

---
+++ cheta-extension/dist/precache-manifest.5192be5a763a6cc6ae445d2ca07041dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c5ba01d742b080b2b2c664eb9ad9968",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb05e37aec5f091e3772",
+    "url": "/static/css/main.6e096779.chunk.css"
+  },
+  {
+    "revision": "12ce8a74a3efc3f06d05",
+    "url": "/static/js/2.657b1a3d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.657b1a3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb05e37aec5f091e3772",
+    "url": "/static/js/main.fa16481d.chunk.js"
+  },
+  {
+    "revision": "f5f555c8e247b214e71c",
+    "url": "/static/js/runtime-main.e71a846f.js"
+  }
+]);

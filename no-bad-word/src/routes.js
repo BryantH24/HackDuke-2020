f33078ce66c2f4efb9d9406bfa426c8fbdccc6b9
George Zhuang <xgz2@cornell.edu>
2020-12-05 21:35:08 -0500

    Save changes

---
+++ no-bad-word/src/routes.js
@@ -0,0 +1,21 @@
+// contains routes to all pages in react app
+import React, { Component } from "react";
+import { Router, Switch, Route } from "react-router-dom";
+import history from "./history";
+import Homepage from "./pages/homepage";
+import editor from "./pages/editor";
+
+// need to add routes for the rest of pages once merged to master
+
+export default class Routes extends Component {
+  render() {
+    return (
+      <Router history={history}>
+        <Switch>
+          <Route path="/" exact component={Homepage} />
+          <Route path="/editor" component={Editor} />
+        </Switch>
+      </Router>
+    );
+  }
+}
